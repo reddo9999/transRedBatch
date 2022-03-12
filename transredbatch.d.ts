6f6353c73bf6d545372a90c28b12849e4c16abd1
reddo9999 <26098031+reddo9999@users.noreply.github.com>
2022-03-11 21:20:17 -0300

    Update version and release

---
+++ transredbatch.d.ts
@@ -0,0 +1,39 @@
+declare class RedBatchTranslatorButton {
+    private panel;
+    private button;
+    private parent;
+    constructor(parent: RedBatchTranslator);
+}
+declare class RedBatchTranslatorWindow {
+    private parent;
+    private container;
+    constructor(parent: RedBatchTranslator);
+    open(): void;
+    close(): void;
+}
+declare class RedBatchTranslatorRow {
+    private location;
+    constructor(file: string, index: number);
+    getValue(): any;
+    isTranslated(): boolean;
+    setValue(text: string, destination: number): void;
+    getTags(): any;
+}
+declare class RedBatchTranslator {
+    private button;
+    private window;
+    constructor();
+    open(): void;
+    close(): void;
+    translateProject(options: {
+        translator: string;
+        destination: number;
+        ignoreTranslated: boolean;
+        blacklist: Array<string>;
+        whitelist: Array<string>;
+        files: Array<string>;
+        strict: boolean;
+        saveOnEachBatch: boolean;
+    }): void;
+}
+declare var thisAddon: any;
