7a1968af0c423e6a2306ffea2e930a8b0aa4fe56
reddo9999 <26098031+reddo9999@users.noreply.github.com>
2022-05-02 16:08:56 -0300

    More translatable scripts

--- ts/RedBatchCheatSheet.ts
+++ ts/RedBatchCheatSheet.ts
@@ -8,8 +8,8 @@ const removableContexts = [
 ];
 
 const translatableNoteRegExp = /(<SG)|(<SAC.+?:)/gim;
-const translatablePluginRegExp = /^(?:DW_(?!SET))|(?:D_TEXT )|(?:addLog )|(?:DW_)|(?:ShowInfo )/gim;
-const translatableCommentRegExp = /^NEVERNEVER$/g;
+const translatablePluginRegExp = /^(?:DW_(?!SET))|(?:D_TEXT )|(?:addLog )|(?:DW_)|(?:ShowInfo )|(^text_indicator :)/gim;
+const translatableCommentRegExp = /^text_indicator :/g;
 const translatablePluginJSRegExp = /[^\x21-\x7E\* ]+/g;
 const translatableControlVariable = /.*/g;
 const translatableVxAceScript = ["Vocab", "装備拡張", "Custom Menu Base"];
